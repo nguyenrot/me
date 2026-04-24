3b282885097b020f2d13414d0f43530a71bdc98d
Nguyen Pham Ky <phamkynguyen753@gmail.com>
2026-04-25 01:16:34 +0700

    chore: auto-deploy 2026-04-25 01:16

--- app/page.tsx
+++ app/page.tsx
@@ -18,6 +18,7 @@ export default function Home() {
         <AboutSection />
         <SkillsSection />
         <ExperienceSection />
+        <SocialSection />
         <LinksSection />
         <Footer />
       </main>
